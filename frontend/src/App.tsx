4f7467abb0dc6182ab2a8d49d372c31c7942f460
joshuahenriques <joshuahenriques95@gmail.com>
2022-02-24 15:30:02 -0500

    containerize both frontend and backend services

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,40 +5,53 @@ import { Pagination } from "./components/Pagination/Pagination";
 import { SearchForm } from "./components/SearchForm/SearchForm";
 import { Query } from "./types/Query";
 import { Header } from "./components/Header/Header";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export const App = () => {
-
-  const [articles, setArticles] = useState([])
-  const [queryy, setQuery] = useState<Query>( { keywords: '', fromDate: '', toDate: '', sort: '', page: 1 })
+  const [articles, setArticles] = useState([]);
+  const [queryy, setQuery] = useState<Query>({
+    keywords: "",
+    fromDate: "",
+    toDate: "",
+    sort: "",
+    page: 1,
+  });
 
   const onSearch = async (query: Query) => {
-    setQuery(query)
-    const res = await fetch(`http://127.0.0.1:5000/search/?keywords=${query.keywords}&from=${query.fromDate}&to=${query.toDate}&sort-by=${query.sort}&page=${query.page}`)
-    const data = await res.json()
-    setArticles(data.articles)
-  }
+    setQuery(query);
+    // console.log(process.env.REACT_APP_FLASK_IP)
+    // const res = await fetch(`http://127.0.0.1:5000/search/?keywords=${query.keywords}&from=${query.fromDate}&to=${query.toDate}&sort-by=${query.sort}&page=${query.page}`)
+    const res = await fetch(
+      `http://${process.env.REACT_APP_FLASK_IP}:8000/search/?keywords=${query.keywords}&from=${query.fromDate}&to=${query.toDate}&sort-by=${query.sort}&page=${query.page}`,
+      { mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin':'*'
+      } 
+      });
+    const data = await res.json();
+    setArticles(data.articles);
+  };
 
   const nextPage = () => {
-    onSearch({...queryy, page: queryy.page + 1})
-  }
+    onSearch({ ...queryy, page: queryy.page + 1 });
+  };
 
   const prevPage = () => {
-    if(queryy.page === 1) {
-      alert("No previous page")
-      return
+    if (queryy.page === 1) {
+      alert("No previous page");
+      return;
     }
-    onSearch({...queryy, page: queryy.page - 1})
-  }
+    onSearch({ ...queryy, page: queryy.page - 1 });
+  };
 
   return (
     <div>
       <Header />
-        <SearchForm onSearch={ onSearch }/>
-        {/* <h3>Keywords: { queryy.keywords } <br></br> FromDate: { queryy.fromDate } <br></br> ToDate: { queryy.toDate } <br></br> sort: { queryy.sort } <br></br> page: { queryy.page }</h3> */}
-        <Pagination query={ queryy } nextPage={ nextPage } prevPage={ prevPage }/>
-        <Articles articles={ articles } />
-        <Pagination query={ queryy } nextPage={ nextPage } prevPage={ prevPage }/>
+      <SearchForm onSearch={onSearch} />
+      {/* <h3>Keywords: { queryy.keywords } <br></br> FromDate: { queryy.fromDate } <br></br> ToDate: { queryy.toDate } <br></br> sort: { queryy.sort } <br></br> page: { queryy.page }</h3> */}
+      <Pagination query={queryy} nextPage={nextPage} prevPage={prevPage} />
+      <Articles articles={articles} />
+      <Pagination query={queryy} nextPage={nextPage} prevPage={prevPage} />
       <Footer />
     </div>
   );
